d4d00e7e10f341a247a98cb543deb7d9fa89cf01
Umar Akhtar <umarakhtarms@gmail.com>
2023-05-08 06:08:30 +0500

    sas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Students from './Pages/Students.js';
 import Fee from './Pages/Fee.js'
 import Classes from './Pages/Classes';
 import AddStudent from './Pages/AddStudent';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
@@ -13,16 +14,16 @@ function App() {
 
   return (
     <>
-      <Router>
+      <BrowserRouter basename="/SMS">
         <Routes>
-          <Route exact path="/" element={<Login/>} />
+          <Route exact path="/SMS" element={<Login/>} />
           <Route exact path="/teachers" element={<Teacher tableHeader={"teacher"} title={"Teachers"} />} />
           <Route exact path="/students" element={<Students tableHeader={"student"} title={"Students"}/>} />
           <Route exact path="/classes" element={<Classes tableHeader={"student"} title={"Students"}/>} />
           <Route exact path="/fee" element={<Fee tableHeader={"fee"} title={"Fee"} />} />
           <Route exact path="/addStudent" element={<AddStudent/>}/>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
