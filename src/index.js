a42b5caf52ef2248cdc849babef5bfc26b47c810
Umar Akhtar <umarakhtarms@gmail.com>
2023-05-08 05:36:49 +0500

    sas

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter basename="/SMS">
-        <App />
+        <h1>WOWOWOWOWO</h1>
     </BrowserRouter>
 );
 
